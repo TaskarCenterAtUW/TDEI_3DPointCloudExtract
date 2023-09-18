f50435dfc2d4b178e920075eba7d7b741e38a65a
obaiddawarki <92597077+obaiddawarki@users.noreply.github.com>
2023-09-18 14:04:20 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Identification of walakable pathways in PointCloud data using computer vision',
+    author='Syed_Obaid',
+    license='MIT',
+)
